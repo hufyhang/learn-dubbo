9850b0c081b2f47d65de167cff27a757a932c6e3
Feifei Hang <hangfeifei@longxintaiye.com>
2021-01-14 08:41:13 +0800

    对Swagger增加application.properties相关的配置支持

--- rest-provider/src/main/java/com/example/restprovider/SwaggerConfig.java
+++ rest-provider/src/main/java/com/example/restprovider/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package com.example.restprovider;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -12,6 +13,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
+@ConditionalOnProperty(name = "swagger.enable", havingValue = "true")
 public class SwaggerConfig {
 
   @Bean
